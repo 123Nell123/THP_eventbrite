8c5f23def6056b07c48576f05c073452650df559
123Nell123 <nellp12321@gmail.com>
2022-02-22 13:52:54 -0800

    from

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,15 +6,8 @@ class UserMailer < ApplicationMailer
     @user = user
     @url  = 'https://eventbrite.herokuapp.com'
  
-    # mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+     mail(to: @user.email,from: 'purhasard@hotmail.com' , subject: 'Welcome to My Awesome Site')
 
-
-
-from = Email.new(email: 'purhasard@hotmail.com')
-to = @user.email
-subject = 'Sending with SendGrid is Fun'
-content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-mail = Mail.new(from, subject, to, content)
-   
+  
   end
 end
