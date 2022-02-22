78e5f5720d7474b136b505861479ba91720d5c2d
123Nell123 <nellp12321@gmail.com>
2022-02-22 13:33:08 -0800

    @user = user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
     default from: 'notifications@example.com'
 
   def welcome_email(user)
- 
+    @user = user
     @url  = 'https://eventbrite.herokuapp.com'
  
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
